2b619be77671e28c486dcfee946cb43cf7392175
tuonidugou <hht@g.ecc.u-tokyo.ac.jp>
2025-10-10 14:14:06 +0900

    7 bits

--- gc/default/default.c
+++ gc/default/default.c
@@ -368,7 +368,7 @@ struct RMoved {
 
 #define RMOVED(obj) ((struct RMoved *)(obj))
 
-typedef uintptr_t bits_t;
+typedef uint64_t bits_t;
 enum {
     BITS_SIZE = sizeof(bits_t),
     BITS_BITLENGTH = ( BITS_SIZE * CHAR_BIT )
@@ -783,7 +783,7 @@ heap_page_in_global_empty_pages_pool(rb_objspace_t *objspace, struct heap_page *
     }
 }
 
-#define GET_PAGE_BODY(x)   ((struct heap_page_body *)((bits_t)(x) & ~(HEAP_PAGE_ALIGN_MASK)))
+#define GET_PAGE_BODY(x)   ((struct heap_page_body *)((uintptr_t)(x) & ~(HEAP_PAGE_ALIGN_MASK)))
 #define GET_PAGE_HEADER(x) (&GET_PAGE_BODY(x)->header)
 #define GET_HEAP_PAGE(x)   (GET_PAGE_HEADER(x)->page)
 
