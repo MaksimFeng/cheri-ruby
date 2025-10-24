a005eab45068bd1026c3f53493972f7f5140f68a
tuonidugou <hht@g.ecc.u-tokyo.ac.jp>
2025-10-24 15:12:28 +0900

    atom revise

--- include/ruby/atomic.h
+++ include/ruby/atomic.h
@@ -41,7 +41,6 @@
 #endif
 
 #include <stdatomic.h>         /* std::atomic */
-#include <machine/atomic.h>
 
 #include "ruby/assert.h"
 #include "ruby/backward/2/limits.h"
@@ -751,9 +750,10 @@ rbimpl_atomic_ptr_exchange(void *volatile *ptr, const void *val)
     return atomic_swap_ptr(ptr, RBIMPL_CAST((void *)val));
 
 	#elif defined(__CHERI_PURE_CAPABILITY__) 
-	_Atomic(void *) *p = (_Atomic(void *) *)ptr;
+	// _Atomic(void *) *p = (_Atomic(void *) *)ptr;
 	void *v = (void *)val;
-	return atomic_exchange(p, v); // return old value
+	// return atomic_exchange(p, v); // return old value
+    return __atomic_exchange_n(ptr, v, __ATOMIC_SEQ_CST);
 
 #else
     RBIMPL_STATIC_ASSERT(sizeof_voidp, sizeof *ptr == sizeof(size_t));
@@ -774,8 +774,9 @@ rbimpl_atomic_value_exchange(volatile VALUE *ptr, VALUE val)
 {
     // RBIMPL_STATIC_ASSERT(sizeof_value, sizeof *ptr == sizeof(size_t));
 	// #if defined(__CHERI_PURE_CAPABILITY__) 
-	_Atomic(VALUE) *atomic_ptr = (_Atomic(VALUE) *)ptr;
-    return atomic_exchange(atomic_ptr, val);
+	// _Atomic(VALUE) *atomic_ptr = (_Atomic(VALUE) *)ptr;
+    // return atomic_exchange(atomic_ptr, val);
+    return __atomic_exchange_n(ptr, val, __ATOMIC_SEQ_CST);
 
     // const size_t sval = RBIMPL_CAST((size_t)val);
     // volatile size_t *const sptr = RBIMPL_CAST((volatile size_t *)ptr);
@@ -912,16 +913,28 @@ rbimpl_atomic_ptr_cas(void **ptr, const void *oldval, const void *newval)
     return atomic_cas_ptr(ptr, pold, pnew);
 
 #elif defined(__CHERI_PURE_CAPABILITY__) 
-	// Define an atomic pointer type compatible with *ptr
-    _Atomic(void *) *atomic_ptr = (_Atomic(void *) *)ptr;
+	// // Define an atomic pointer type compatible with *ptr
+    // _Atomic(void *) *atomic_ptr = (_Atomic(void *) *)ptr;
 
-    void *expected = (void *)oldval;
+    // void *expected = (void *)oldval;
 
-    // atomic_compare_exchange_strong updates expected if the comparison fails
-    (void)atomic_compare_exchange_strong(atomic_ptr, &expected, (void *)newval);
+    // // atomic_compare_exchange_strong updates expected if the comparison fails
+    // atomic_compare_exchange_strong_explicit(
+    //     atomic_ptr,
+    //     &expected,
+    //     (void *)newval,
+    //     memory_order_seq_cst,
+    //     memory_order_seq_cst
+    // );
 
-    // Return the previous value (whether or not the exchange succeeded)
-    return expected;
+    // // Return the previous value (whether or not the exchange succeeded)
+    // return expected;
+	void **pptr = RBIMPL_CAST((void **)ptr);
+	void *pold = RBIMPL_CAST((void *)oldval);
+    void *pnew = RBIMPL_CAST((void *)newval);
+	__atomic_compare_exchange_n(
+        pptr, &pold, pnew, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+    return pold;
 
 #else
     RBIMPL_STATIC_ASSERT(sizeof_voidp, sizeof *ptr == sizeof(size_t));
@@ -957,16 +970,26 @@ RBIMPL_ATTR_NONNULL((1))
 static inline VALUE
 rbimpl_atomic_value_cas(volatile VALUE *ptr, VALUE oldval, VALUE newval)
 {
-	#if defined(__CHERI_PURE_CAPABILITY__) 
-	// Treat ptr as an atomic VALUE pointer
-    _Atomic(VALUE) *atomic_ptr = (_Atomic(VALUE) *)ptr;
+#if defined(__CHERI_PURE_CAPABILITY__) 
+    // // Treat ptr as an atomic VALUE pointer
+    // _Atomic(VALUE) *atomic_ptr = (_Atomic(VALUE) *)ptr;
+    // VALUE expected = oldval;
 
-    VALUE expected = oldval;
+    // // Attempt CAS operation and ignore the boolean result
+    // atomic_compare_exchange_strong_explicit(
+    //     atomic_ptr,
+    //     &expected,
+    //     newval,
+    //     memory_order_seq_cst,
+    //     memory_order_seq_cst
+    // );
 
-    // Attempt CAS; if fails, expected is updated to current *ptr
-    (void)atomic_compare_exchange_strong(atomic_ptr, &expected, newval);
+    // // Return the prior value (either oldval on success, or the current one on failure)
+    // return expected;
+	VALUE expected = oldval;
 
-    // Return the prior value (either oldval on success, or the current one on failure)
+	__atomic_compare_exchange_n(
+        ptr, &expected, newval, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
     return expected;
 	#else
     RBIMPL_STATIC_ASSERT(sizeof_value, sizeof *ptr == sizeof(size_t));
